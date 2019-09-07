44505e12164666d97f4984cab4f48cea594cec36
alex <anoshkin.allex@gmail.com>
2019-09-07 22:47:21 +0300

    перенес путь до корня в настройки

--- src/test/java/com/example/webflux_templates/BookControllerTest.java
+++ src/test/java/com/example/webflux_templates/BookControllerTest.java
@@ -1,27 +1,42 @@
 package com.example.webflux_templates;
 
+import com.example.webflux_templates.models.Book;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.reactive.server.EntityExchangeResult;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
 import java.util.List;
 
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class BookControllerTest {
+
+    @Value("${apiRootPath}")
+    private String rootApi;
 
     @Autowired
     private WebTestClient webTestClient;
 
     @Test
     public void allBooks() {
-        webTestClient
-                .get().uri("/books")
+        EntityExchangeResult<List> result = webTestClient
+                .get().uri(rootApi + "/books")
                 .exchange()
                 .expectStatus().isOk()
-                .expectBody(List.class);
+                .expectBody(List.class)
+                .returnResult();
+
+        List<Book> books = result.getResponseBody();
+
+        assertNotNull(books);
+        assertTrue(books.size() > 0);
     }
 }
